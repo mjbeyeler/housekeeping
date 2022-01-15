e1fd0077ff5d31d5982d74b4ce407e6b706d86eb
mjbeyeler <mjbeyeler@protonmail.ch>
2022-01-15 14:42:56 +0100

    config adapted for brynhild

--- config.sh
+++ config.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-ARCHIVE_DIR=/stornext/CHUV1/archive/FAC/FBM/DBC/sbergman/
-SCRATCH_DIR=/scratch/beegfs/FAC/FBM/DBC/sbergman/
+# Jura
+#ARCHIVE_DIR=/stornext/CHUV1/archive/FAC/FBM/DBC/sbergman/
+#SCRATCH_DIR=/scratch/beegfs/FAC/FBM/DBC/sbergman/
+
+# brynhild
+ARCHIVE_DIR=/HDD/data/
+SCRATCH_DIR=/NVME/scratch/
